bc3de3c5e0800bd0a691e435ad168eec97f1b33a
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-29 20:33:27 -0500

    First Commit

---
+++ AreYouOldEnoughToVote/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreYouOldEnoughToVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 18)
+            {
+                Console.WriteLine("You are not old enough to vote!");
+            }
+
+            else
+            {
+                Console.WriteLine("You are old enough to vote!");
+            }
+        }
+    }
+}
